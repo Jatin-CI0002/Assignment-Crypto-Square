98c26d4da0514dccfb254014873f7eb2575b4e97
Jatin-CI0002 <jatin@caizin.com>
2022-09-14 16:05:58 +0530

    corrections in previous commit

--- app.js
+++ app.js
@@ -3,92 +3,116 @@ let inputString = document.getElementById("string");
 let key = document.getElementById("key");
 let encryptedDisplay = (document.getElementById("encrypt-display-string"));
 let decryptedDisplay = (document.getElementById("decrypt-display-string"));
-let encButton = document.getElementById("encrypt");
-let decButton = document.getElementById("decrypt");
-let matrix = [[], []];
-let spaceArray = [];
+const encButton = document.getElementById("encrypt");
+const decButton = document.getElementById("decrypt");
+const matrix = [[], []];
+const spaceArray = [];
 let encryptedString = "";
-function encrypt(event) {
+// ENCRYPT function started
+function encrypt() {
     let str = inputString.value;
-    str = str.replace("%20", " ");
-    str = str.replace(/[^a-zA-Z]/g, " ");
-    str = str.toLowerCase();
+    // if my fields is empty then encrypt function will not work
     if (!str || +key.value < 3) {
         return;
     }
+    // storing index of whitespaces into array and characters into another string to use further
     let originalString = "";
     let spaceCount = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str.charAt(i) === " ") {
-            spaceArray.push(i - spaceCount);
+    for (let index = 0; index < str.length; index++) {
+        if (str.charAt(index) === " ") {
+            spaceArray.push(index - spaceCount);
             spaceCount++;
         }
         else
-            originalString += str.charAt(i);
+            originalString += str.charAt(index);
     }
-    let matrixLength = parseInt(key.value);
+    // pushing characters into the matrix to encode given string
+    const matrixLength = +key.value;
     let iterator = 0;
-    for (let i = 0; i < originalString.length; i++) {
-        matrix[i] = [];
-        for (let j = 0; j < matrixLength; j++) {
-            matrix[i].push(originalString.charAt(iterator++));
+    for (let row = 0; row < originalString.length; row++) {
+        matrix[row] = [];
+        for (let col = 0; col < matrixLength; col++) {
+            matrix[row].push(originalString.charAt(iterator++));
         }
     }
-    for (let i = 0; i < matrix[0].length; i++) {
-        for (let j = 0; j < matrix.length; j++) {
-            encryptedString += matrix[j][i];
+    // creating the encoded string with the help of above matrix
+    for (let row = 0; row < matrix[0].length; row++) {
+        for (let col = 0; col < matrix.length; col++) {
+            encryptedString += matrix[col][row];
         }
     }
-    let encodingString = "";
+    // creating encode for spaces and adding them with encoded string
+    let encodedString = "";
     spaceArray.forEach((item) => {
-        encodingString += item + "-";
+        encodedString += item + "-";
     });
-    let encoding = btoa(encodingString);
-    inputString.value = "";
+    const encoding = btoa(encodedString);
     encryptedString += "|" + encoding;
     encryptedDisplay.innerHTML = `Your encrypted string is "${encryptedString}"`;
     encryptedDisplay.style.display = "block";
 }
+// DECRYPT function started
 function decrypt() {
-    let decryptionKey = +key.value;
-    let string = [];
-    if (!encryptedString)
-        string = inputString.value.split("|");
-    else
-        string = encryptedString.split("|");
-    // string that needs to be decrypted
-    let stringLength = string[0].length;
-    let decryptionString = string[0];
-    // array containing spaces
-    let indexString = atob(string[1]);
+    let string = "";
+    let spaceString = "";
+    let index = 0;
+    if (!encryptedString) {
+        index = inputString.value.lastIndexOf("|");
+        string = inputString.value.substring(0, index);
+        spaceString = inputString.value.substring(index);
+    }
+    else {
+        index = encryptedString.lastIndexOf("|");
+        string = encryptedString.substring(0, index);
+        spaceString = encryptedString.substring(index + 1);
+    }
+    // if the fields are null then function will not propagate further
+    if (!string || +key.value < 3) {
+        return;
+    }
+    // storing string that needs to be decrypted
+    const stringLength = string.length;
+    let decryptionString = string;
+    // creating array that will contain white spaces
+    const indexString = atob(spaceString);
     let spaceIndex = indexString.split("-");
     let decryptedString = "";
-    let matrix = [[], []];
-    let columnLength = Math.floor(stringLength / decryptionKey);
+    const matrix = [[], []];
+    // getting lengths for the matrix to decode the encoded string
+    const decryptionKey = +key.value;
+    const columnLength = Math.floor(stringLength / decryptionKey);
     let columnRemainder = stringLength % decryptionKey;
+    let increment = 0;
     let spaceIterator = 0;
-    for (let i = 0; i < decryptionString.length; i++) {
-        matrix[i] = [];
-        for (let j = 0; j < columnLength + columnRemainder; j++) {
-            matrix[i].push(decryptionString.charAt(spaceIterator++));
+    if (columnRemainder > 0)
+        increment = 1;
+    // creating matrix for decoded string  
+    for (let row = 0; row < decryptionString.length; row++) {
+        matrix[row] = [];
+        for (let col = 0; col < columnLength + increment; col++) {
+            matrix[row].push(decryptionString.charAt(spaceIterator++));
         }
-        if (columnRemainder != 0)
+        if (columnRemainder > 0)
             columnRemainder--;
+        if (columnRemainder <= 0)
+            increment = 0;
     }
-    for (let i = 0; i < matrix[0].length; i++) {
-        for (let j = 0; j < matrix.length; j++) {
-            if (matrix[j][i] !== undefined)
-                decryptedString += matrix[j][i];
+    // getting all characters from matrix to create decoded string
+    for (let row = 0; row < matrix[0].length; row++) {
+        for (let col = 0; col < matrix.length; col++) {
+            if (matrix[col][row] !== undefined)
+                decryptedString += matrix[col][row];
         }
     }
+    // adding all the whitespaces into decoded string at required places
     let iterator = 0;
     let finalString = "";
-    for (let i = 0; i < decryptedString.length; i++) {
-        if (i == +spaceIndex[iterator]) {
+    for (let index = 0; index < decryptedString.length; index++) {
+        if (index == +spaceIndex[iterator]) {
             finalString += " ";
             iterator++;
         }
-        finalString += decryptedString.charAt(i);
+        finalString += decryptedString.charAt(index);
     }
     decryptedDisplay.innerHTML = `Your decrypted string is "${finalString}"`;
     decryptedDisplay.style.display = "block";
--- app.ts
+++ app.ts
@@ -4,107 +4,145 @@ let key = <HTMLInputElement>document.getElementById("key");
 let encryptedDisplay = <HTMLInputElement>(document.getElementById("encrypt-display-string"));
 let decryptedDisplay = <HTMLInputElement>(document.getElementById("decrypt-display-string"));
 
-let encButton = <HTMLInputElement>document.getElementById("encrypt");
-let decButton = <HTMLInputElement>document.getElementById("decrypt");
+const encButton = <HTMLInputElement>document.getElementById("encrypt");
+const decButton = <HTMLInputElement>document.getElementById("decrypt");
 
-let matrix: string[][] = [[], []];
-let spaceArray: number[] = [];
+const matrix: string[][] = [[], []];
+const spaceArray: number[] = [];
 
 let encryptedString: string = "";
 
-function encrypt(event: Event): void {
+// ENCRYPT function started
+function encrypt(): void {
+
   let str: string = inputString.value;
 
-  str = str.replace("%20", " ");
-  str = str.replace(/[^a-zA-Z]/g, " ");
-  str = str.toLowerCase();
-
+  // if my fields is empty then encrypt function will not work
   if (!str || +key.value < 3) {
     return;
   }
 
+  // storing index of whitespaces into array and characters into another string to use further
+
   let originalString: string = "";
   let spaceCount: number = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (str.charAt(i) === " ") {
-      spaceArray.push(i - spaceCount);
+
+  for (let index = 0; index < str.length; index++) {
+    if (str.charAt(index) === " ") {
+      spaceArray.push(index - spaceCount);
       spaceCount++;
     } 
     else 
-      originalString += str.charAt(i);
+      originalString += str.charAt(index);
   }
-  let matrixLength = parseInt(key.value);
+
+  // pushing characters into the matrix to encode given string
+  const matrixLength = +key.value;
   let iterator = 0;
 
-  for (let i = 0; i < originalString.length; i++) {
-    matrix[i] = [];
-    for (let j = 0; j < matrixLength; j++) {
-      matrix[i].push(originalString.charAt(iterator++));
+  for (let row = 0; row < originalString.length; row++) {
+    matrix[row] = [];
+    for (let col = 0; col < matrixLength; col++) {
+      matrix[row].push(originalString.charAt(iterator++));
     }
   }
 
-  for (let i = 0; i < matrix[0].length; i++) {
-    for (let j = 0; j < matrix.length; j++) {
-      encryptedString += matrix[j][i];
+  // creating the encoded string with the help of above matrix
+  for (let row = 0; row < matrix[0].length; row++) {
+    for (let col = 0; col < matrix.length; col++) {
+      encryptedString += matrix[col][row];
     }
   }
-  let encodingString = "";
+
+  // creating encode for spaces and adding them with encoded string
+  let encodedString = "";
   spaceArray.forEach((item) => {
-    encodingString += item + "-";
+    encodedString += item + "-";
   });
-  let encoding = btoa(encodingString);
+
+  const encoding = btoa(encodedString);
   
-  inputString.value = "";
   encryptedString += "|" + encoding;
   encryptedDisplay.innerHTML = `Your encrypted string is "${encryptedString}"`;
   encryptedDisplay.style.display = "block";
 }
 
+
+// DECRYPT function started
 function decrypt(): void {
-  let decryptionKey = +key.value;
-  let string: string[] = [];
-  if (!encryptedString) 
-    string = inputString.value.split("|");
-  else 
-    string = encryptedString.split("|");
 
-  // string that needs to be decrypted
-  let stringLength = string[0].length;
-  let decryptionString = string[0];
-  
-  // array containing spaces
-  let indexString = atob(string[1]);
+  let string: string = "";
+  let spaceString = "";
+  let index = 0;
+
+  if(!encryptedString){
+    index = inputString.value.lastIndexOf("|");
+    string = inputString.value.substring(0,index);
+    spaceString = inputString.value.substring(index);
+  }
+  else{ 
+    index = encryptedString.lastIndexOf("|");
+    string = encryptedString.substring(0,index);
+    spaceString = encryptedString.substring(index + 1);
+  }
+
+  // if the fields are null then function will not propagate further
+  if (!string || +key.value < 3) {
+    return;
+  }
+  // storing string that needs to be decrypted
+
+  const stringLength = string.length;
+  let decryptionString = string;
+
+  // creating array that will contain white spaces
+
+  const indexString = atob(spaceString);
   let spaceIndex = indexString.split("-");
-
+  
   let decryptedString = "";
-  let matrix: string[][] = [[], []];
-
-  let columnLength = Math.floor(stringLength / decryptionKey);
+  const matrix: string[][] = [[], []];
+  
+  // getting lengths for the matrix to decode the encoded string
+  const decryptionKey = +key.value;
+  const columnLength = Math.floor(stringLength / decryptionKey);
   let columnRemainder = stringLength % decryptionKey;
-
+  let increment = 0;
   let spaceIterator = 0;
-  for (let i = 0; i < decryptionString.length; i++) {
-    matrix[i] = [];
-    for (let j = 0; j < columnLength + columnRemainder; j++) {
-      matrix[i].push(decryptionString.charAt(spaceIterator++));
+
+  if(columnRemainder > 0)
+    increment = 1;
+
+  // creating matrix for decoded string  
+  for (let row = 0; row < decryptionString.length; row++) {
+    matrix[row] = [];
+    for (let col = 0; col < columnLength + increment; col++) {
+      matrix[row].push(decryptionString.charAt(spaceIterator++));
     }
-    if (columnRemainder != 0) columnRemainder--;
+    if (columnRemainder > 0)
+    columnRemainder--;
+
+   if(columnRemainder <= 0)
+     increment = 0;
   }
 
-  for (let i = 0; i < matrix[0].length; i++) {
-    for (let j = 0; j < matrix.length; j++) {
-      if (matrix[j][i] !== undefined) 
-        decryptedString += matrix[j][i];
+  // getting all characters from matrix to create decoded string
+  for (let row = 0; row < matrix[0].length; row++) {
+    for (let col = 0; col < matrix.length; col++) {
+      if (matrix[col][row] !== undefined) 
+        decryptedString += matrix[col][row];
     }
   }
+
+  // adding all the whitespaces into decoded string at required places
   let iterator = 0;
   let finalString = "";
-  for (let i = 0; i < decryptedString.length; i++) {
-    if (i == +spaceIndex[iterator]) {
+  for (let index = 0; index < decryptedString.length; index++) {
+    if (index == +spaceIndex[iterator]) {
       finalString += " ";
       iterator++;
     }
-    finalString += decryptedString.charAt(i);
+    finalString += decryptedString.charAt(index);
   }
   decryptedDisplay.innerHTML = `Your decrypted string is "${finalString}"`;
   decryptedDisplay.style.display = "block";
